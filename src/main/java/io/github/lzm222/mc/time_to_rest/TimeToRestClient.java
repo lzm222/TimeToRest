1d552d73a262b4b98c2a4bf121eeddd500b200a5
lzm222 <194671393+lzm222@users.noreply.github.com>
2026-08-11 10:37:32 +0800

    init:删除无用模板代码

--- src/main/java/io/github/lzm222/mc/time_to_rest/TimeToRestClient.java
+++ src/main/java/io/github/lzm222/mc/time_to_rest/TimeToRestClient.java
@@ -1,31 +1,20 @@
 package io.github.lzm222.mc.time_to_rest;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = TimeToRest.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = TimeToRest.MODID, value = Dist.CLIENT)
+// @EventBusSubscriber(modid = TimeToRest.MODID, value = Dist.CLIENT)
 public class TimeToRestClient {
     public TimeToRestClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-    }
-
-    @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        TimeToRest.LOGGER.info("HELLO FROM CLIENT SETUP");
-        TimeToRest.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
