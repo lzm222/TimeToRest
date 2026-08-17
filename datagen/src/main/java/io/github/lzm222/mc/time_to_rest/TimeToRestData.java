d627567d881e5ce60c5fb84585e263737532f396
lzm222 <194671393+lzm222@users.noreply.github.com>
2026-08-17 11:21:31 +0800

    refactor:分离datagen

---
+++ datagen/src/main/java/io/github/lzm222/mc/time_to_rest/TimeToRestData.java
@@ -0,0 +1,8 @@
+package io.github.lzm222.mc.time_to_rest;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.Mod;
+
+@Mod(value = TimeToRestCommon.MODID, dist = Dist.CLIENT)
+public class TimeToRestData {
+}
